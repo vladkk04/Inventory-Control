df8bc6ec79135548d8ac9a554d09ce1c43f3c187
Vladyslav Klymiuk <vladuslaw512@gmail.com>
2025-01-05 20:44:04 +0100

    update

--- app/src/main/java/com/example/bachelorwork/ui/fragments/orderList/create/OrderAddProductModalBottomSheetFragment.kt
+++ app/src/main/java/com/example/bachelorwork/ui/fragments/orderList/create/OrderAddProductModalBottomSheetFragment.kt
@@ -1,11 +1,9 @@
 package com.example.bachelorwork.ui.fragments.orderList.create
 
-import android.graphics.Color
-import android.provider.CalendarContract.Colors
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -17,76 +15,97 @@ import com.example.bachelorwork.databinding.ProductItemInOrderSelectionBinding
 import com.example.bachelorwork.ui.collectInLifecycle
 import com.example.bachelorwork.ui.common.base.BaseBottomSheetDialogFragment
 import com.example.bachelorwork.ui.model.OrderSelectableProductUi
+import com.example.bachelorwork.ui.utils.extensions.toDp
 import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
-class OrderAddProductModalBottomSheetFragment: BaseBottomSheetDialogFragment<FragmentModalBottomSheetOrderAddProductBinding>()
-{
+class OrderAddProductModalBottomSheetFragment :
+    BaseBottomSheetDialogFragment<FragmentModalBottomSheetOrderAddProductBinding>() {
 
-    override val bindingInflater : (LayoutInflater,ViewGroup?,Boolean) -> FragmentModalBottomSheetOrderAddProductBinding
+    override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentModalBottomSheetOrderAddProductBinding
         get() = FragmentModalBottomSheetOrderAddProductBinding::inflate
 
-    override val isAllowFullScreen : Boolean = false
+    override val isAllowFullScreen: Boolean = false
 
-    override val isFullScreen : Boolean = false
+    override val isFullScreen: Boolean = false
 
-    private val viewModel : OrderAddProductViewModel by viewModels()
+    private val viewModel: OrderAddProductViewModel by viewModels()
 
-    private val adapter : SimpleBindingAdapter<OrderSelectableProductUi> =
-        simpleAdapter<OrderSelectableProductUi,ProductItemInOrderSelectionBinding> {
-            bind { product ->
-                this.textViewName.text = product.name
+    private val adapter: SimpleBindingAdapter<OrderSelectableProductUi> =
+        simpleAdapter<OrderSelectableProductUi, ProductItemInOrderSelectionBinding> {
+            areItemsSame = { oldItem, newItem -> oldItem.id == newItem.id }
+            areContentsSame = { oldItem, newItem -> oldItem == newItem && index(oldItem) == index(newItem) }
+
+            bind { item ->
+                this.textViewName.text = item.name
+
+                Log.d("debug", "a ${item}")
+
+                if (item.isSelected) {
+                    this.layout.alpha = 0.5f
+                    this.cardView.strokeWidth = 2.toDp(requireContext())
+                    showAllOtherComponents()
+                    Log.d("debug", "d")
+                } else {
+                    this.layout.alpha = 1f
+                    this.cardView.strokeWidth = 0
+                    hideAllComponents()
+                    Log.d("debug", "c")
+                }
             }
             listeners {
-                this.root.apply {
-                    setOnClickListener {
-                        alpha = 0.5f
-                        setBackgroundColor(Color.RED)
-                        showAllOtherComponents()
-                    }
+                root.onClick {
+                    Log.d("debug", "f ${index()}")
+                    viewModel.selectItem(index())
                 }
-
-                this.checkboxDetail.onClick { product ->
+                checkboxDetail.onClick { product ->
                     viewModel.navigateToProductDetail(product.id)
                 }
             }
         }
 
-    override fun setupViews()
-    {
+
+    override fun setupViews() {
         setupSearchBar()
         setupRecycleView()
+        setupInputForm()
+        setupAddButton()
 
         collectInLifecycle(viewModel.uiState) {
             setupUiState(it)
         }
-
+        collectInLifecycle(viewModel.uiFormState) {
+            setupFormUiState(it)
+        }
     }
 
-    private fun setupUiState(uiState : OrderAddProductUiState)
-    {
-        if (uiState.searchQuery.isNotEmpty())
-        {
+
+    private fun setupUiState(uiState: OrderAddProductUiState) {
+        if (uiState.products.isEmpty() && uiState.searchQuery.isNotEmpty()) {
+            binding.textViewEnterOrScan.text = getString(R.string.text_no_result)
+        } else {
+            binding.textViewEnterOrScan.text = getString(R.string.text_enter_or_scan)
+        }
+
+        if (uiState.searchQuery.isNotEmpty() && uiState.products.isNotEmpty()) {
             adapter.submitList(uiState.products) {
                 binding.recyclerViewProducts.visibility = View.VISIBLE
                 binding.textViewEnterOrScan.visibility = View.INVISIBLE
             }
-        } else
-        {
-            binding.recyclerViewProducts.visibility = View.INVISIBLE
-            binding.textViewEnterOrScan.visibility = View.INVISIBLE
-            hideAllComponents()
         }
     }
 
-    private fun setupRecycleView()
-    {
+    private fun setupFormUiState(uiState: OrderAddProductFormUiState) {
+        binding.textInputLayoutQuantity.error = uiState.quantityError
+        binding.textInputLayoutRate.error = uiState.rateError
+    }
+
+    private fun setupRecycleView() {
         with(binding.recyclerViewProducts) {
             setHasFixedSize(true)
             adapter = this@OrderAddProductModalBottomSheetFragment.adapter
-            layoutManager =
-                LinearLayoutManager(requireContext(),LinearLayoutManager.HORIZONTAL,false)
+            layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
         }
     }
 
@@ -96,6 +115,21 @@ class OrderAddProductModalBottomSheetFragment: BaseBottomSheetDialogFragment<Fra
         }
         binding.textInputLayoutSearch.setEndIconOnClickListener {
             viewModel.scanBarcodeScanner()
+        }
+    }
+
+    private fun setupInputForm() {
+        binding.textInputLayoutQuantity.editText?.doAfterTextChanged {
+            viewModel.onEvent(OrderAddProductFormEvent.Quantity(it.toString()))
+        }
+        binding.textInputLayoutRate.editText?.doAfterTextChanged {
+            viewModel.onEvent(OrderAddProductFormEvent.Rate(it.toString()))
+        }
+    }
+
+    private fun setupAddButton() {
+        binding.buttonAddProduct.setOnClickListener {
+            viewModel.addProduct()
         }
     }
 
